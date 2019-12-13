b401bc8ec5f67d12be8eddf0dc254c1f2c9722c5
minkolee <minkolee@gmail.com>
2019-12-13 15:55:44 +0800

    启动成功了EhCache, 但不知道为何看起来没生效

--- src/main/java/cc/conyli/service/CustomerServiceImpl.java
+++ src/main/java/cc/conyli/service/CustomerServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-@CacheConfig(cacheNames = "users")
+@CacheConfig(cacheNames = "ehentai")
 public class CustomerServiceImpl implements CustomerService {
 
     private CustomerDAO customerDAO;
