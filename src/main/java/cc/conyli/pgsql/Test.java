8f28229cc2794f952427c77cc9811256620e838f
minkolee <minkolee@gmail.com>
2019-12-24 23:45:19 +0800

    连接PgSQL成功

--- src/main/java/cc/conyli/pgsql/Test.java
+++ src/main/java/cc/conyli/pgsql/Test.java
@@ -1,6 +1,30 @@
 package cc.conyli.pgsql;
 
+import java.io.*;
+import java.sql.*;
+import java.util.Properties;
+
 public class Test {
 
+    public static void main(String[] args) throws IOException, SQLException {
 
+//        String url = "jdbc:postgresql://106.54.215.164/mydb";
+//        Properties props = new Properties();
+//        props.setProperty("user","minkolee");
+//        props.setProperty("password","fflym0709");
+//        props.setProperty("currentSchema","test");
+//        Connection conn = DriverManager.getConnection(url, props);
+
+        String url = "jdbc:postgresql://106.54.215.164/mydb?user=minkolee&password=fflym0709&currentSchema=test";
+        Connection conn = DriverManager.getConnection(url);
+
+        System.out.println(conn);
+
+        String sql = "SELECT * FROM test.department";
+        Statement statement = conn.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString(1));
+        }
+    }
 }
